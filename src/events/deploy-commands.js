2b1da556e3ca31a44e52796611150748829fdd7a
Nicholas <contactnicholas81@gmail.com>
2025-04-21 00:47:22 +0100

    Update deploy-commands.js

--- src/events/deploy-commands.js
+++ src/events/deploy-commands.js
@@ -37,7 +37,7 @@ const rest = new REST().setToken(process.env.TOKEN);
     );
 
     const data = await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
+      Routes.applicationCommands(clientId),
       { body: commands }
     );
 
